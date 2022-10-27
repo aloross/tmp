8572e816a1b09e8440dd25c61adc9dddf48c93d3
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2022-10-27 12:43:42 +0200

    Add gql codegen

--- packages/apps/front/app/page.tsx
+++ packages/apps/front/app/page.tsx
@@ -1,27 +1,26 @@
-import { gql } from '@apollo/client'
-import client from '@tfm4/lib'
+import client from "@tfm4/lib";
+import { graphql } from "../gql";
 
-const GET_RESTAURANT = gql`
+const GET_RESTAURANT = graphql(`
   query getRestaurant($restaurantUuid: String!) {
     restaurant(restaurantUuid: $restaurantUuid) {
       name
-      __typename
     }
   }
-`
+`);
 
 export default async function Home() {
-  const restaurant = await getData()
-  return <div>{restaurant.name}</div>
+  const restaurant = await getData();
+  return <div>{restaurant.name}</div>;
 }
 
 async function getData() {
   const { data } = await client.query({
     query: GET_RESTAURANT,
     variables: {
-      restaurantUuid: '97cf5aee-ef48-4787-9661-30562fd593d1',
+      restaurantUuid: "97cf5aee-ef48-4787-9661-30562fd593d1",
     },
-  })
+  });
 
-  return data.restaurant
+  return data.restaurant;
 }
