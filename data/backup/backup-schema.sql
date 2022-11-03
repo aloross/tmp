3796f7daa46d2fb340fd0d1648562d13e2c6a087
bdecline <bdeglane@users.noreply.github.com>
2022-11-03 10:04:43 +0100

    wip

--- data/backup/backup-schema.sql
+++ data/backup/backup-schema.sql
@@ -1,75 +1,112 @@
 SET check_function_bodies = false;
 CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
     LANGUAGE plpgsql
-AS $$
+    AS $$
 DECLARE
-    _new record;
+  _new record;
 BEGIN
-    _new := NEW;
-    _new."updated_at" = NOW();
-    RETURN _new;
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
 END;
 $$;
 CREATE FUNCTION public.set_slug_from_name() RETURNS trigger
     LANGUAGE plpgsql
-AS $$
+    AS $$
 BEGIN
-    NEW.slug := slugify(NEW.name);
-    RETURN NEW;
+  NEW.slug := slugify(NEW.name);
+  RETURN NEW;
 END
 $$;
 CREATE FUNCTION public.slugify(value text) RETURNS text
     LANGUAGE sql IMMUTABLE
-AS $_$
-    -- removes accents (diacritic signs) from a given string --
-WITH "unaccented" AS (
+    AS $_$
+  -- removes accents (diacritic signs) from a given string --
+  WITH "unaccented" AS (
     SELECT unaccent("value") AS "value"
-),
-     -- lowercases the string
-     "lowercase" AS (
-         SELECT lower("value") AS "value"
-         FROM "unaccented"
-     ),
-     -- remove single and double quotes
-     "removed_quotes" AS (
-         SELECT regexp_replace("value", '[''"]+', '', 'gi') AS "value"
-         FROM "lowercase"
-     ),
-     -- replaces anything that's not a letter, number, hyphen('-'), or underscore('_') with a hyphen('-')
-     "hyphenated" AS (
-         SELECT regexp_replace("value", '[^a-z0-9\\-_]+', '-', 'gi') AS "value"
-         FROM "removed_quotes"
-     ),
-     -- trims hyphens('-') if they exist on the head or tail of the string
-     "trimmed" AS (
-         SELECT regexp_replace(regexp_replace("value", '\-+$', ''), '^\-', '') AS "value"
-         FROM "hyphenated"
-     )
-SELECT "value" FROM "trimmed";
+  ),
+  -- lowercases the string
+  "lowercase" AS (
+    SELECT lower("value") AS "value"
+    FROM "unaccented"
+  ),
+  -- remove single and double quotes
+  "removed_quotes" AS (
+    SELECT regexp_replace("value", '[''"]+', '', 'gi') AS "value"
+    FROM "lowercase"
+  ),
+  -- replaces anything that's not a letter, number, hyphen('-'), or underscore('_') with a hyphen('-')
+  "hyphenated" AS (
+    SELECT regexp_replace("value", '[^a-z0-9\\-_]+', '-', 'gi') AS "value"
+    FROM "removed_quotes"
+  ),
+  -- trims hyphens('-') if they exist on the head or tail of the string
+  "trimmed" AS (
+    SELECT regexp_replace(regexp_replace("value", '\-+$', ''), '^\-', '') AS "value"
+    FROM "hyphenated"
+  )
+  SELECT "value" FROM "trimmed";
 $_$;
+CREATE TABLE public.accounts (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    type text NOT NULL,
+    provider text NOT NULL,
+    "providerAccountId" text NOT NULL,
+    refresh_token text,
+    access_token text,
+    expires_at bigint,
+    token_type text,
+    scope text,
+    id_token text,
+    session_state text,
+    oauth_token_secret text,
+    oauth_token text,
+    "userId" uuid NOT NULL,
+    refresh_token_expires_in integer
+);
 CREATE TABLE public.customer (
-                                 id uuid DEFAULT public.gen_random_uuid() NOT NULL,
-                                 data jsonb
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    data jsonb
 );
 CREATE TABLE public.reservation (
-                                    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
-                                    created_at timestamp with time zone DEFAULT now() NOT NULL,
-                                    updated_at timestamp with time zone DEFAULT now() NOT NULL,
-                                    date timestamp with time zone NOT NULL,
-                                    status text NOT NULL,
-                                    customer_id uuid,
-                                    restaurant_id uuid
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    date timestamp with time zone NOT NULL,
+    status text NOT NULL,
+    customer_id uuid,
+    restaurant_id uuid
 );
 CREATE TABLE public.reservation_status_enum (
     value text NOT NULL
 );
 CREATE TABLE public.restaurant (
-                                   id uuid DEFAULT public.gen_random_uuid() NOT NULL,
-                                   created_at timestamp with time zone DEFAULT now() NOT NULL,
-                                   updated_at timestamp with time zone DEFAULT now() NOT NULL,
-                                   name text NOT NULL,
-                                   slug text NOT NULL
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    name text NOT NULL,
+    slug text NOT NULL
 );
+CREATE TABLE public.sessions (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    "sessionToken" text NOT NULL,
+    "userId" uuid NOT NULL,
+    expires timestamp with time zone
+);
+CREATE TABLE public.users (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text,
+    email text,
+    "emailVerified" timestamp with time zone,
+    image text
+);
+CREATE TABLE public.verification_tokens (
+    token text NOT NULL,
+    identifier text NOT NULL,
+    expires timestamp with time zone
+);
+COPY public.accounts (id, type, provider, "providerAccountId", refresh_token, access_token, expires_at, token_type, scope, id_token, session_state, oauth_token_secret, oauth_token, "userId", refresh_token_expires_in) FROM stdin;
+\.
 COPY public.customer (id, data) FROM stdin;
 becbe285-63b3-44a0-903c-f16c0c187ed4	\N
 dd92dcc0-f7a1-420c-a28b-d1b2735b3a19	\N
@@ -89,6 +126,14 @@ COPY public.restaurant (id, created_at, updated_at, name, slug) FROM stdin;
 2d95c42b-dea7-4977-95f5-9a1d20c7afdd	2022-10-28 12:39:33.667994+00	2022-10-28 12:39:33.667994+00	salut les loulous	salut-les-loulous
 2fd08528-d754-4854-8710-797d2299cca3	2022-10-28 12:41:05.301624+00	2022-10-28 12:41:05.301624+00	la grande fête	la-grande-fete
 \.
+COPY public.sessions (id, "sessionToken", "userId", expires) FROM stdin;
+\.
+COPY public.users (id, name, email, "emailVerified", image) FROM stdin;
+\.
+COPY public.verification_tokens (token, identifier, expires) FROM stdin;
+\.
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.customer
     ADD CONSTRAINT customer_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.reservation
@@ -97,14 +142,26 @@ ALTER TABLE ONLY public.reservation_status_enum
     ADD CONSTRAINT reservation_status_enum_pkey PRIMARY KEY (value);
 ALTER TABLE ONLY public.restaurant
     ADD CONSTRAINT restaurant_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.verification_tokens
+    ADD CONSTRAINT verification_tokens_pkey PRIMARY KEY (token);
 CREATE TRIGGER restaurant_slug_insert BEFORE INSERT ON public.restaurant FOR EACH ROW WHEN (((new.name IS NOT NULL) AND (new.slug IS NULL))) EXECUTE FUNCTION public.set_slug_from_name();
 CREATE TRIGGER set_public_reservation_updated_at BEFORE UPDATE ON public.reservation FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_reservation_updated_at ON public.reservation IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 CREATE TRIGGER set_public_restaurant_updated_at BEFORE UPDATE ON public.restaurant FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_restaurant_updated_at ON public.restaurant IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT "accounts_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 ALTER TABLE ONLY public.reservation
     ADD CONSTRAINT reservation_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES public.customer(id);
 ALTER TABLE ONLY public.reservation
     ADD CONSTRAINT reservation_restaurant_id_fkey FOREIGN KEY (restaurant_id) REFERENCES public.restaurant(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.reservation
     ADD CONSTRAINT reservation_status_fkey FOREIGN KEY (status) REFERENCES public.reservation_status_enum(value) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE CASCADE;
