dec49e366238ee920c22a52ee760a18240c8bc43
bdecline <bdeglane@users.noreply.github.com>
2022-11-04 16:51:00 +0100

    remove

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,16 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
+const { HASURA_GRAPHQL_ADMIN_SECRET } = process.env
+
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:8081/v1/graphql',
+  schema: {
+    'http://localhost:8081/v1/graphql': {
+      headers: {
+        'X-Hasura-Admin-Secret': HASURA_GRAPHQL_ADMIN_SECRET as string,
+      },
+    },
+  },
   documents: 'packages/apps/front/pages/**/*.graphql',
   generates: {
     'packages/generated/src/index.ts': {
