84736478e4e6ab67b21757ef756a0a189ff0deea
bdecline <bdeglane@users.noreply.github.com>
2022-11-07 11:15:59 +0100

    Move bootstrap workflow code as explicit example package

--- packages/apps/action/src/example.ts
+++ packages/apps/action/src/example.ts
@@ -1,9 +1,9 @@
 import { Handler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import { ExampleSchema, ExampleParams } from '@tfm4/worker-reservation'
-import { createReservationAction } from '@tfm4/action-reservation'
+import { ExampleSchema, ExampleParams } from '@tfm4/worker-example'
+import { exampleActionStart } from '@tfm4/action-example'
 import { ZodError } from 'zod'
 
-export const createReservation: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const exampleAction: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // 1. get request body from serverless event
   const body: ExampleParams = event?.body ? JSON.parse(event.body) : {}
   // 2. validate request body
@@ -18,7 +18,7 @@ export const createReservation: Handler = async (event: APIGatewayProxyEvent): P
 
   // 3. fire and forget or return the result of the temporal action
   try {
-    const result = await createReservationAction(body)
+    const result = await exampleActionStart(body)
 
     return {
       statusCode: 200,
