621fa656b66752365726f1ddd857202895af4c85
Benoît Deglane <bdeglane@users.noreply.github.com>
2022-11-16 17:38:47 +0100

    Add request-id among logs and apps (#23)

--- packages/apps/action/src/reservation/get-by-day.ts
+++ packages/apps/action/src/reservation/get-by-day.ts
@@ -1,31 +1,6 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda'
+import { Handler } from 'aws-lambda'
 import { reservation } from '@tfm4/temporal-client'
-import { ZodError } from 'zod'
 import { GetReservationsByDayParams, GetReservationsByDaySchema } from '@tfm4/domain-reservation'
+import { createHandler } from '../create-handler'
 
-export const getReservationByDay: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const params = event?.pathParameters
-
-  try {
-    GetReservationsByDaySchema.parse(params)
-  } catch (e) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ issues: (e as ZodError).issues }),
-    }
-  }
-
-  try {
-    const reservations = await reservation.getReservationByDay(params as unknown as GetReservationsByDayParams)
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(reservations),
-    }
-  } catch (e) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({}),
-    }
-  }
-}
+export const getReservationByDay: Handler = createHandler<GetReservationsByDayParams>(GetReservationsByDaySchema, reservation.getReservationByDay, false)
--- packages/client/src/availability/computeAvailabilityClient.ts
+++ packages/client/src/availability/computeAvailabilityClient.ts
@@ -2,5 +2,5 @@ import { taskQueue } from '@tfm4/config'
 import { computeAvailability, ComputeAvailabilityParams } from '@tfm4/domain-availability'
 import { createClient } from '../create-client'
 
-export const computeAvailabilityClient = async (params: ComputeAvailabilityParams) => createClient<ComputeAvailabilityParams>(taskQueue.AVAILABILITY, computeAvailability, params)
+export const computeAvailabilityClient = async (params: ComputeAvailabilityParams, requestId: string) => createClient<ComputeAvailabilityParams>(taskQueue.AVAILABILITY, computeAvailability, params, requestId)
 
